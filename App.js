9d8afb99b9dc188feda3c76bb820866da9343129
Seema Yousuf Zai <72159369+webprogrammer124@users.noreply.github.com>
2020-12-01 13:05:04 +0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,19 @@
+import{Button}from '@material-ui/core'
+import './App.css';
+import Api from './components/Api';
+import Covid from './components/Api';
+import MediaCard from './components/MediaCard';
+
+
+function App() {
+  return (
+    <div className="App">
+     <Api />
+  
+      
+
+    </div>
+  );
+}
+
+export default App;
